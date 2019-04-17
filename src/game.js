172dabf7bbbdd97240cc6e7dbe177bc738a9cca6
warbelt <warbelt.dev@gmail.com>
2019-04-17 23:41:51 +0200

    Initial commit

---
+++ src/game.js
@@ -0,0 +1,38 @@
+const Die = require("./die.js")
+
+class Game {
+    constructor() {
+        this.dice_list = []
+        this.results = []
+    }
+
+    restartResults() {
+        this.results = []
+    }
+
+    addDie(faces_list) {
+        var die = new Die(faces_list)
+        this.dice_list.push(die)
+    }
+
+    getDiceList() {
+        for (var dice in this.dice_list) {
+            console.log(dice)
+        }
+    }
+
+    rollDice() {
+        var roll = []
+        this.dice_list.forEach(
+            function(dice) {
+                roll.push(dice.roll())
+        })
+        this.results.push(roll)
+    }
+
+    getResults() {
+        return this.results
+    }
+}
+
+module.exports = Game
