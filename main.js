e85b0872660b496754cc16f88471db4911cef474
Mario Durántez <warbelt.dev@gmail.com>
2019-04-22 18:16:35 +0200

    [Dev] Fix onClose() attached to win instead of app

--- main.js
+++ main.js
@@ -6,10 +6,15 @@ let win // Global reference to the window so the GC does not close it
 function createWindow () {
     win = new BrowserWindow({ width: 1280, height: 720 })
     win.loadURL('file://' + __dirname + '/app/www/index.html');
-
-    win.on('closed', () => {
-        win = null
-    })
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
+
+app.on('closed', () => {
+    win.removeAllListeners('close')
+    win = null
+});
