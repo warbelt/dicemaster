1da7a6cb7d2b044192d5621796f12ed088e9211b
Mario Durántez <warbelt.dev@gmail.com>
2019-05-22 18:40:14 +0200

    [Dev] Set chromeless window with drag and close interaction

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,5 @@
 const inlineSVG = require("inline-svg");
+const remote = require("electron").remote;
 
 const Controller = require("./controller/controller.js");
 const Model = require("./model/model.js");
@@ -8,6 +9,11 @@ const InputParser = require("./inputParser.js");
 document.addEventListener("DOMContentLoaded", initialize);
 
 function initialize() {
+    document.querySelector(".close").addEventListener("click", function (e) {
+        var window = remote.getCurrentWindow();
+        window.close();
+   }); 
+
     var inputParser = new InputParser();
     var model = new Model(inputParser);
     var view = new View(model);
